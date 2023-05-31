1c67630983f026ecc3ed7381cebc2815e01b1a9e
Chia Yu Hsin Sarah <sarah.chia8795@gmail.com>
2023-05-31 11:10:15 +0800

    Initial Commit

---
+++ github linking/github_linkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_linkingApp.swift
+//  github linking
+//
+//  Created by Sarah Chia on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct github_linkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
